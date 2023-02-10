8c63241573a54738c56793a94f815277d45a63c7
Joseph Morales Silva <josephzx1@hotmail.com>
2023-02-09 20:35:27 -0600

    add search icon

--- src/TodoSearch/index.jsx
+++ src/TodoSearch/index.jsx
@@ -1,8 +1,9 @@
 import { useContext } from "react";
 import { TodoContext } from "../TodoContext";
+import { FaSearch } from "react-icons/fa";
 
-export function TodoSearch() {
-  const {searchValue, setSearchValue}=useContext(TodoContext)
+export function TodoSearch({ placeholder = "Buscar" }) {
+  const { searchValue, setSearchValue } = useContext(TodoContext);
   const onSearchValueChange = (event) => {
     const { value } = event.target;
     setSearchValue(value);
@@ -11,16 +12,18 @@ export function TodoSearch() {
     <div className="flex flex-col relative ">
       <div
         className="
-    absolute inset-1
-    bg-gradient-to-r from-primary to-quaternary 
-    rounded-lg blur opacity-10 -ml-2 -mt-2        
-    "
+          absolute inset-1
+          bg-gradient-to-r from-primary to-quaternary 
+          rounded-lg blur opacity-10 -ml-2 -mt-2        
+        "
       ></div>
       <div className="relative bg-white rounded-lg flex items-center align-middle border">
+        <FaSearch className="text-primary pl-2 h-5 w-8 flex-shrink" />
+
         <input
           className="ml-2 py-3 pl-2 h-full w-full rounded-lg placeholder-primary placeholder-opacity-70 focus:outline-none focus:ring focus:ring-inset focus:border-quaternary "
           type="text"
-          placeholder="Cebolla"
+          placeholder={placeholder}
           value={searchValue} //React pide que los el valor del input este conectados al estado
           onChange={onSearchValueChange}
         />
